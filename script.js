51bd561cbd0dcebe709f39ae41d6e66fedbb9756
Cor9Kase <cornelius@skardigital.no>
2025-03-12 13:22:06 +0100

    script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Google Script URL - Erstatt denne med din egen URL fra Google Apps Script
-const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwFA5Kxga-ZkMLcNLONBO0AJfBfjNOKRLUQ5UMO8fW4dtE1TIsu2rFXOOXSIjwxzXuC/exec';
+const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwjCOdv0P2fkWP4O9RsGHlbZ5yN-8oIQwpCWEOYVq9hs9E0AqiL9l6bbeEKMNDSsQFu/exec';
 
 // Store active timers and their data
 const timers = {};
