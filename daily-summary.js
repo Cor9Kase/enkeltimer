9be1a6c48e317655d259eb067aa67491a4394e5b
Cor9Kase <cornelius@skardigital.no>
2025-03-12 10:11:36 +0100

    Update daily-summary.js

--- daily-summary.js
+++ daily-summary.js
@@ -1,5 +1,5 @@
 // Google Script URL - Samme URL som i hovedapplikasjonen
-const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwcymjRTK47gv0YFxiiBMstKaGADDZPWxaV4FivcZKIPktNd_JAKHEeMfdLOzLOx3oW/exec';
+const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzDgVM_CGD57M8jrxC2IhGQQ_iYsTkMOKGXV2jfJ0kgsYf25HdbZOfQfaE7iI4JwxTD/exec';
 
 // Variabler for datoer og data
 let currentMonth = new Date().getMonth();
