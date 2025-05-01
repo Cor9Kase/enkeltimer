11dd744d7e528ed661f439d6ad7308feaab83cd4
Cor9Kase <cornelius@skardigital.no>
2025-05-01 14:55:51 +0200

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function debounce(func, wait) {
 }
 
 // Google Script URL - *** VIKTIG: Bytt ut med din egen publiserte URL ***
-const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzlUqfCMJ3L_ZXB9dYzZTwPqohjEyfiwzFCuZBIz3i7F8N1cI0w0U6dN2ED7O4UhTts/exec'; // <--- SETT INN DIN URL HER!
+const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbz_jkk-sZznH12XAhRM86xdCSKYkZedTBgwXD2fVd25iM7j7WgwpuWdEOcypRDREHcl/exec'; // <--- SETT INN DIN URL HER!
 
 // Globale variabler for tilstand
 const timers = {};
