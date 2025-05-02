6cf495ecf3ac37e65a30ab931d4f2698b21ffbdc
Cor9Kase <cornelius@skardigital.no>
2025-05-02 11:48:51 +0200

    Update tasks.js

--- tasks.js
+++ tasks.js
@@ -1,7 +1,7 @@
 // tasks.js - Logikk for oppgavesiden
 
 // Google Script URL - *** VIKTIG: MÅ VÆRE SAMME SOM I ANDRE JS-FILER ***
-const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycby0rrJxVUZnIb7ZPXyASYP6qhv2H9nEaLN4ti2IOb-uqSfjGSqDKZBgiWVwM0w6mo4_/exec'; // <-- SETT INN DIN URL HER!
+const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxpfYJg1haeFXvPxGbZaKB_9VEizTelyA5Qb5lW0knZEhiU5FQENxX0i0oc3jZEb_V9/exec'; // <-- SETT INN DIN URL HER!
 
 // Globale variabler for oppgavehåndtering
 let allTasks = []; // Holder alle hentede oppgaver
